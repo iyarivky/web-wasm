27e4804958572c90cc02998f027e042936c23cd2
Iya Rivvikyn <101973571+iyarivky@users.noreply.github.com>
2023-06-27 12:39:40 +0000

    pusing njir

---
+++ example.js
@@ -0,0 +1,225 @@
+function parseUrls(vrayUrls) {
+    const vrayUrlsLength = vrayUrls.length;
+    const results = [];
+  
+    const parseVmessUrl = (vrayParsedUrl) => {
+      let href = vrayParsedUrl.href
+      let encoded = href.substring(8);
+      let decodeResult = atob(encoded);
+      let parsedJSON = JSON.parse(decodeResult);
+      const configResult = {
+        tag: parsedJSON.ps,
+        type: "vmess",
+        server: parsedJSON.add,
+        server_port: parseInt(parsedJSON.port, 10),
+        uuid: parsedJSON.id,
+        security: "auto",
+        alter_id: parsedJSON.aid,
+        global_padding: false,
+        authenticated_length: true,
+        multiplex: {
+          enable: false,
+          protocol: "smux",
+          max_streams: 32
+        }
+      };
+  
+      if (parsedJSON.port === "443" || parsedJSON.tls === "tls") {
+        configResult.tls = {
+          enable: true,
+          server_name: parsedJSON.sni || parsedJSON.add,
+          insecure: true,
+          disable_sni: false
+        };
+      }
+  
+      if (parsedJSON.net === "ws") {
+        configResult.transport = {
+          type: parsedJSON.net,
+          path: parsedJSON.path,
+          headers: {
+            Host: parsedJSON.host || parsedJSON.add
+          }
+        };
+      } else if (parsedJSON.net === "grpc") {
+        configResult.transport = {
+          type: parsedJSON.net,
+          service_name: parsedJSON.path
+        };
+      }
+  
+      return configResult;
+    };
+  
+    const parseVlessUrl = (vrayParsedUrl) => {
+      const configResult = {
+        tag: vrayParsedUrl.hash.substring(1),
+        type: "vless",
+        server: vrayParsedUrl.hostname,
+        server_port: parseInt(vrayParsedUrl.port, 10),
+        uuid: vrayParsedUrl.username,
+        flow: "",
+        packet_encoding: "xudp",
+        multiplex: {
+          enable: false,
+          protocol: "smux",
+          max_streams: 32
+        }
+      };
+  
+      if (vrayParsedUrl.port === "443" || vrayParsedUrl.searchParams.get("security") === "tls") {
+        configResult.tls = {
+          enable: true,
+          server_name: vrayParsedUrl.searchParams.get("sni"),
+          insecure: true,
+          disable_sni: false
+        };
+      }
+  
+      const transportTypes = {
+        ws: {
+          type: vrayParsedUrl.searchParams.get("type"),
+          path: vrayParsedUrl.searchParams.get("path"),
+          headers: {
+            Host: vrayParsedUrl.searchParams.get("host")
+          }
+        },
+        grpc: {
+          type: vrayParsedUrl.searchParams.get("type"),
+          service_name: vrayParsedUrl.searchParams.get("serviceName")
+        }
+      };
+  
+      configResult.transport = transportTypes[vrayParsedUrl.searchParams.get("type")];
+  
+      return configResult;
+    };
+  
+    const parseTrojanUrl = (vrayParsedUrl) => {
+      const configResult = {
+        tag: vrayParsedUrl.hash.substring(1),
+        type: "trojan",
+        server: vrayParsedUrl.hostname,
+        server_port: parseInt(vrayParsedUrl.port, 10),
+        password: vrayParsedUrl.username,
+        multiplex: {
+          enable: false,
+          protocol: "smux",
+          max_streams: 32
+        }
+      };
+  
+      if (vrayParsedUrl.port === "443" || vrayParsedUrl.searchParams.get("security") === "tls") {
+        configResult.tls = {
+          enable: true,
+          server_name: vrayParsedUrl.searchParams.get("sni"),
+          insecure: true,
+          disable_sni: false
+        };
+      }
+  
+      const transportTypes = {
+        ws: {
+          type: vrayParsedUrl.searchParams.get("type"),
+          path: vrayParsedUrl.searchParams.get("path"),
+          headers: {
+            Host: vrayParsedUrl.searchParams.get("host")
+          }
+        },
+        grpc: {
+          type: vrayParsedUrl.searchParams.get("type"),
+          service_name: vrayParsedUrl.searchParams.get("serviceName")
+        }
+      };
+  
+      configResult.transport = transportTypes[vrayParsedUrl.searchParams.get("type")];
+  
+      return configResult;
+    };
+  
+    const parseShadowsocksUrl = (vrayParsedUrl) => {
+      const configResult = {
+        tag: vrayParsedUrl.hash.replace("#", ""),
+        type: vrayParsedUrl.protocol.replace(":", ""),
+        server: vrayParsedUrl.hostname,
+        server_port: parseInt(vrayParsedUrl.port, 10)
+      };
+      return configResult;
+    };
+    const parseShadowsocksRUrl = (vrayParsedUrl) => {
+      const configResult = {
+        tag: vrayParsedUrl.hash.replace("#", ""),
+        type: vrayParsedUrl.protocol.replace(":", ""),
+        server: vrayParsedUrl.hostname,
+        server_port: parseInt(vrayParsedUrl.port, 10)
+      };
+      return configResult;
+    };
+    const parseSocksUrl = (vrayParsedUrl) => {
+      const configResult = {
+        tag: vrayParsedUrl.hash.replace("#", ""),
+        type: vrayParsedUrl.protocol.replace(":", ""),
+        server: vrayParsedUrl.hostname,
+        server_port: parseInt(vrayParsedUrl.port, 10)
+      };
+      return configResult;
+    };
+    const parseHttpUrl = (vrayParsedUrl) => {
+      const configResult = {
+        tag: vrayParsedUrl.hash.replace("#", ""),
+        type: vrayParsedUrl.protocol.replace(":", ""),
+        server: vrayParsedUrl.hostname,
+        server_port: parseInt(vrayParsedUrl.port, 10)
+      };
+      return configResult;
+    };
+    // for (let i = 0; i < urlStrings.length; i++)
+    // for (const urlString of urlStrings) <= slow
+  
+    const protocolMap = {
+      "vmess:": parseVmessUrl,
+      "vless:": parseVlessUrl,
+      "trojan:": parseTrojanUrl,
+      "ss:": parseShadowsocksUrl,
+      "ssr:": parseShadowsocksRUrl,
+      "socks5:": parseSocksUrl,
+      "http:": parseHttpUrl
+    };
+    
+    for (let i = 0; i < vrayUrlsLength; i++) {
+      const vrayString = vrayUrls[i];
+      const vrayParsedUrl = new URL(vrayString);
+    
+      let configResult;
+    
+      const protocolHandler = protocolMap[vrayParsedUrl.protocol];
+      if (protocolHandler) {
+        configResult = protocolHandler(vrayParsedUrl);
+      } else {
+        console.log("Unsupported Protocol!")
+      }
+      const panjangResult = results.length;
+      results[panjangResult] = configResult;
+    }
+    return results;
+  }
+  
+  const vrayUrls = [
+    "vmess://eyJhZGQiOiAic2cyLXJheS5pcHNlcnZlcnMueHl6IiwgImhvc3QiOiAic25pLmNsb3VkZmxhcmUuY29tIiwgImFpZCI6IDAsICJ0eXBlIjogIiIsICJwYXRoIjogIi9KQUdPQU5TU0gvIiwgIm5ldCI6ICJ3cyIsICJwcyI6ICJqYWdvYW5zc2gtZ29kZGFtbiIsICJ0bHMiOiAidGxzIiwgInR5cGUiOiAibm9uZSIsICJwb3J0IjogIjQ0MyIsICJ2IjogIjIiLCAiaWQiOiAiNGE0NWU0NzctY2ZhMS00YTBmLWEwYjAtZTQ1MTczYzYyZjViIn0=",
+    "vmess://eyJhZGQiOiJ1czIub2NlaXMubmV0IiwiYWlkIjoiMCIsImFscG4iOiIiLCJmcCI6IiIsImhvc3QiOiIiLCJpZCI6ImRhY2Y2MzQwLTA5ZmQtMTFlZS1iMjM2LTIwNWM2ZDVmNWQ3OCIsIm5ldCI6IndzIiwicGF0aCI6Ii92bXdzIiwicG9ydCI6IjQ0MyIsInBzIjoiVVNBK1ZNRVNTLVdTKDIwMjMtMDYtMjApIiwic2N5Ijoibm9uZSIsInNuaSI6IndoYXRzYXBwLm5ldCIsInRscyI6InRscyIsInR5cGUiOiIiLCJ2IjoiMiJ9",
+    "vmess://eyJhZGQiOiJ1czIub2NlaXMubmV0IiwiYWlkIjoiMCIsImFscG4iOiIiLCJmcCI6IiIsImhvc3QiOiIiLCJpZCI6ImRhY2Y2MzQwLTA5ZmQtMTFlZS1iMjM2LTIwNWM2ZDVmNWQ3OCIsIm5ldCI6IndzIiwicGF0aCI6Ii92bXdzIiwicG9ydCI6IjgwIiwicHMiOiJVU0ErVk1FU1MtV1MgTlRMUygyMDIzLTA2LTIwKSIsInNjeSI6Im5vbmUiLCJzbmkiOiIiLCJ0bHMiOiIiLCJ0eXBlIjoiIiwidiI6IjIifQ==",
+    "vless://a771070c-b93e-4f72-8747-657f4a41ead9@sglws.mainssh.xyz:443?path=/vless&security=tls&encryption=none&host=sglws.mainssh.xyz&type=ws&sni=sglws.mainssh.xyz#mainssh-legendo",
+    "vless://d4cbf663-6950-4c64-8a52-f8b82a02e031@sg4-ws.xvless.xyz:80?path=%2Fwebsocket&security=none&encryption=none&host=sg4-ws.xvless.xyz&type=ws&sni=sni.cloudflare.net#sshocean-legendaplis",
+    "vless://f746cd36-f565-444b-8bb8-6043c79eb3da@id1-grpc.xvless.xyz:443?mode=gun&security=tls&encryption=none&type=grpc&serviceName=grpc&sni=sni.cloudflare.net#sshocean-legendano",
+    "trojan://a758ef8c-f06c-4e9d-8f0b-51633db51817@idt6.sshocean.net:443?security=tls&headerType=none&type=tcp&sni=sni.cloudflare.net#sshocean-kenapalegenda_trojan",
+    "trojan://6d9fdac3-d74b-435f-aa6b-5fbf36e06853@sg1.xvless.xyz:443?host=sg1.xvless.xyz&path=%2Ftrojan&sni=sg1.xvless.xyz&type=ws#sshocean-ainian",
+    "trojan://dbedf072-d917-41cd-b106-3aa3bb2f29a4@idt4.sshocean.net:443?mode=gun&security=tls&type=grpc&serviceName=grpc&sni=sni.cloudflare.net#sshocean-pengentest_Trojan_gRPC"
+  ];
+  
+  const startTime = performance.now();
+  const results = parseUrls(vrayUrls);
+  const endTime = performance.now();
+  let diff = endTime - startTime;
+  const jsonResult = JSON.stringify(results, null, 4);
+  console.log(jsonResult);
+  console.log(diff, "ms");
--- rust-unstable-convert/src/main.rs
+++ rust-unstable-convert/src/main.rs
@@ -1,30 +1,15 @@
 use url::Url;
-use serde::{Serialize, Deserialize};
+use serde_derive::{Serialize, Deserialize};
+use serde_json;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct ConfigResult {
-    tag: String,
-    r#type: String,
-    server: String,
-    server_port: u32,
-    uuid: String,
-    security: String,
-    alter_id: String,
-    global_padding: bool,
-    authenticated_length: bool,
-    multiplex: MultiplexInfo,
-    tls: Option<TlsInfo>,
-    transport: Option<TransportInfo>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 struct MultiplexInfo {
     enable: bool,
     protocol: String,
-    max_streams: u32,
+    max_stream: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 struct TlsInfo {
     enable: bool,
     server_name: String,
@@ -32,165 +17,75 @@ struct TlsInfo {
     disable_sni: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 struct TransportInfo {
     r#type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     path: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     headers: Option<HeadersInfo>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     service_name: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
+#[allow(non_snake_case)]
 struct HeadersInfo {
-    Host: Option<String>,
+    Host: String,
 }
 
-fn parse_urls(vray_urls: Vec<String>, http_urls: Vec<String>) -> Vec<ConfigResult> {
-    let results: Vec<ConfigResult> = vray_urls
-        .into_iter()
-        .zip(http_urls)
-        .map(|(vray_string, http_string)| {
-            let vray_parsed_url = Url::parse(&vray_string).unwrap();
-            let http_parsed_url = Url::parse(&http_string).unwrap();
-
-            let config_result = match vray_parsed_url.scheme() {
-                "vmess" => parse_vmess_url(&vray_parsed_url, &http_parsed_url),
-                "vless" => parse_vless_url(&vray_parsed_url, &http_parsed_url),
-                "trojan" => parse_trojan_url(&vray_parsed_url, &http_parsed_url),
-                "ss" => parse_shadowsocks_url(&vray_parsed_url, &http_parsed_url),
-                "ssr" => parse_shadowsocksr_url(&vray_parsed_url, &http_parsed_url),
-                "socks5" => parse_socks_url(&vray_parsed_url, &http_parsed_url),
-                "http" => parse_http_url(&vray_parsed_url, &http_parsed_url),
-                _ => {
-                    println!("Unsupported Protocol!");
-                    return None;
-                }
-            };
-
-            config_result
-        })
-        .filter_map(|result| result)
-        .collect();
-
-    results
+#[derive(Serialize, Deserialize)]
+struct VmessUrlInfo {
+    tag: String,
+    r#type: String,
+    server: String,
+    server_port: u16,
+    uuid: String,
+    security: String,
+    alter_id: u16,
+    global_padding: bool,
+    authenticated_length: bool,
+    packet_encoding: String,
+    tls: Option<TlsInfo>,
+    transport: Option<TransportInfo>,
+    multiplex: MultiplexInfo,
 }
 
-fn parse_vmess_url(vray_parsed_url: &Url, _http_parsed_url: &Url) -> Option<ConfigResult> {
-    let encoded = vray_parsed_url.as_str()[8..].to_owned();
-    let decode_result = base64::decode(&encoded).unwrap();
-    let parsed_json: serde_json::Value = serde_json::from_slice(&decode_result).unwrap();
-
-    let mut config_result = ConfigResult {
-        tag: parsed_json["ps"].as_str()?.to_owned(),
-        r#type: "vmess".to_owned(),
-        server: parsed_json["add"].as_str()?.to_owned(),
-        server_port: parsed_json["port"].as_u64()? as u32,
-        uuid: parsed_json["id"].as_str()?.to_owned(),
-        security: "auto".to_owned(),
-        alter_id: parsed_json["aid"].as_u64()?.to_string(),
-        global_padding: false,
-        authenticated_length: true,
-        multiplex: MultiplexInfo {
-            enable: false,
-            protocol: "smux".to_owned(),
-            max_streams: 32,
-        },
-        tls: None,
-        transport: None,
-    };
-
-    if parsed_json["port"].as_str()? == "443" || parsed_json["tls"].as_str()? == "tls" {
-        config_result.tls = Some(TlsInfo {
-            enable: true,
-            server_name: parsed_json["sni"].as_str()?.to_owned(),
-            insecure: true,
-            disable_sni: false,
-        });
-    }
-
-    if parsed_json["net"].as_str()? == "ws" {
-        config_result.transport = Some(TransportInfo {
-            r#type: Some(parsed_json["net"].as_str()?.to_owned()),
-            path: Some(parsed_json["path"].as_str()?.to_owned()),
-            headers: Some(HeadersInfo {
-                Host: Some(parsed_json["host"].as_str()?.to_owned()),
-            }),
-            service_name: None,
-        });
-    } else if parsed_json["net"].as_str()? == "grpc" {
-        config_result.transport = Some(TransportInfo {
-            r#type: Some(parsed_json["net"].as_str()?.to_owned()),
-            path: None,
-            headers: None,
-            service_name: Some(parsed_json["path"].as_str()?.to_owned()),
-        });
-    }
-
-    Some(config_result)
+#[derive(Serialize, Deserialize)]
+struct VlessUrlInfo {
+    tag: String,
+    r#type: String,
+    server: String,
+    server_port: u16,
+    uuid: String,
+    flow: String,
+    packet_encoding: String,
+    security: String,
+    multiplex: MultiplexInfo,
+    tls: Option<TlsInfo>,
+    transport: Option<TransportInfo>,
 }
 
-fn parse_vless_url(vray_parsed_url: &Url, http_parsed_url: &Url) -> Option<ConfigResult> {
-    let config_result = ConfigResult {
-        tag: http_parsed_url.fragment().map_or_else(|| "".to_owned(), |s| s[1..].to_owned()),
-        r#type: "vless".to_owned(),
-        server: http_parsed_url.host_str()?.to_owned(),
-        server_port: http_parsed_url.port()?,
-        uuid: http_parsed_url.username().to_owned(),
-        security: "".to_owned(),
-        alter_id: "".to_owned(),
-        global_padding: false,
-        authenticated_length: false,
-        multiplex: MultiplexInfo {
-            enable: false,
-            protocol: "smux".to_owned(),
-            max_streams: 32,
-        },
-        tls: None,
-        transport: None,
-    };
-
-    if http_parsed_url.port() == Some(443) || http_parsed_url.query_pairs().any(|(k, _)| k == "security" && _ == "tls") {
-        let sni = http_parsed_url.query_pairs().find(|(k, _)| k == "sni").map(|(_, v)| v.to_owned());
-        let tls_info = TlsInfo {
-            enable: true,
-            server_name: sni.clone().unwrap_or_else(|| http_parsed_url.host_str().unwrap().to_owned()),
-            insecure: true,
-            disable_sni: false,
-        };
-
-        Some(config_result)
-    } else {
-        Some(config_result)
-    }
+#[derive(Serialize, Deserialize)]
+struct TrojanUrlInfo {
+    tag: String,
+    r#type: String,
+    server: String,
+    server_port: u16,
+    password: String,
+    transport: Option<TransportInfo>,
+    tls: Option<TlsInfo>,
+    multiplex: MultiplexInfo,
 }
 
-// Implement the other parsing functions (parse_trojan_url, parse_shadowsocks_url, etc.) similarly
+fn parse_urls(vray_urls: Vec<String>) -> Vec<ConfigResult> {
 
-fn main() {
-    let vray_urls = vec![
-        "vmess://eyJhZGQiOiAic2cyLXJheS5pcHNlcnZlcnMueHl6IiwgImhvc3QiOiAic25pLmNsb3VkZmxhcmUuY29tIiwgImFpZCI6IDAsICJ0eXBlIjogIiIsICJwYXRoIjogIi9KQUdPQU5TU0gvIiwgIm5ldCI6ICJ3cyIsICJwcyI6ICJqYWdvYW5zc2gtZ29kZGFtbiIsICJ0bHMiOiAidGxzIiwgInR5cGUiOiAibm9uZSIsICJwb3J0IjogIjQ0MyIsICJ2IjogIjIiLCAiaWQiOiAiNGE0NWU0NzctY2ZhMS00YTBmLWEwYjAtZTQ1MTczYzYyZjViIn0=",
-        "vless://a771070c-b93e-4f72-8747-657f4a41ead9@sglws.mainssh.xyz:443?path=/vless&security=tls&encryption=none&host=sglws.mainssh.xyz&type=ws&sni=sglws.mainssh.xyz#mainssh-legendo",
-        "trojan://6d9fdac3-d74b-435f-aa6b-5fbf36e06853@sg1.xvless.xyz:443?host=sg1.xvless.xyz&path=%2Ftrojan&sni=sg1.xvless.xyz&type=ws#sshocean-ainian",
-        "trojan://dbedf072-d917-41cd-b106-3aa3bb2f29a4@idt4.sshocean.net:443?mode=gun&security=tls&type=grpc&serviceName=grpc&sni=sni.cloudflare.net#sshocean-pengentest_Trojan_gRPC",
-    ];
-
-    let http_urls = vray_urls
-        .iter()
-        .map(|url_string| url_string.replace_range(..url_string.find(':').unwrap_or(0), "ftp"))
-        .collect::<Vec<_>>();
-
-    let start_time = Instant::now();
-    let results = parse_urls(&vray_urls, &http_urls);
-    let end_time = Instant::now();
-    let diff = end_time.duration_since(start_time);
-    let json_result = serde_json::to_string_pretty(&results).unwrap();
-    println!("{}", json_result);
-    println!("{:?} ms", diff.as_millis());
 }
 
-Note: This Rust code assumes that you have the `url` and `serde_json` crates added to your `Cargo.toml` file. You can add them like this:
-
-```toml
-[dependencies]
-url = "2.2.2"
-serde_json = "1.0.64"
+fn vmess_urls()
+fn vless_urls()
+fn trojan_urls()
+fn shadowsocks_urls()
+fn shadowsocksr_urls()
+fn socks_urls()
+fn http_urls()
